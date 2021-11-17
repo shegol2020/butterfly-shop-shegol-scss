b5621df1dd6b2e2af772e92442df2ae6dea694d9
shegol2020 <shegol2007@gmail.com>
2021-11-17 21:52:44 +0300

    scripts

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,13 +1,24 @@
 $(document).ready(function () {
 
+  $('.burger').on('click', function () {
 
+    $('.nav-menu').slideToggle();
+  });
 
 
   //переключение в контактах
   $('.contacts-list-link').on('click', function (event) {
     event.preventDefault();
+
     $('.contacts-list-link').removeClass('active');
     $(this).addClass('active');
+
+    let index = $(this).index('.contacts-list-link');
+
+    $('.contact-info').removeClass('active');
+    $('.contact-info').eq(index).addClass('active');
   });
+
+
 
 });
