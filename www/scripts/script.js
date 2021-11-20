dcd1bb60d48b99347b4e02ed38073fc4de7bd48f
shegol2020 <shegol2007@gmail.com>
2021-11-20 22:11:08 +0300

    catalog and faq

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -20,5 +20,28 @@ $(document).ready(function () {
   });
 
 
+  $('.filter-link').on('click', function (e) {
+    e.preventDefault();
 
+    let linkType = $(this).data('type');
+
+    $('.filter-link').removeClass('active');
+    $(this).addClass('active');
+
+    if (linkType === 'all') {
+      $('.portfolio-item').show();
+      return;
+    }
+
+    $('.portfolio-item').each(function () {
+      let portfolioType = $(this).data('type');
+
+      if (linkType === portfolioType) {
+        $(this).show();
+        return;
+      }
+
+      $(this).hide();
+    });
+  });
 });
