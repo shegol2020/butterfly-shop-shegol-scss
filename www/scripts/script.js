90b4df66085390a10969e44c6cb064dc60fd0ccc
shegol2020 <shegol2007@gmail.com>
2021-11-26 21:18:27 +0300

    slider

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -29,11 +29,11 @@ $(document).ready(function () {
     $(this).addClass('active');
 
     if (linkType === 'all') {
-      $('.portfolio-item').show();
+      $('.catalog-portfolio-item').show();
       return;
     }
 
-    $('.portfolio-item').each(function () {
+    $('.catalog-portfolio-item').each(function () {
       let portfolioType = $(this).data('type');
 
       if (linkType === portfolioType) {
@@ -44,4 +44,29 @@ $(document).ready(function () {
       $(this).hide();
     });
   });
+
+  // Аккордеон
+  let prevIndex;
+
+  $('.faq-button').on('click', function () {
+    let currentIndex = $(this).index('.faq-button');
+
+    if (currentIndex === prevIndex) {
+      $(this).next().slideToggle();
+      $(this).toggleClass('open');
+      return;
+    }
+
+    $(this).next().slideDown();
+    $(this).addClass('open');
+    $('.faq-button').eq(prevIndex).next().slideUp();
+    $('.faq-button').eq(prevIndex).removeClass('open');
+    prevIndex = currentIndex;
+  });
+
+
+  //slider
+  $('.review-list').slick();
+
+
 });
