daa89db8f77a2d4eabc54522eea86613ed8acbc3
shegol2020 <shegol2007@gmail.com>
2021-12-08 10:56:39 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,56 +8,56 @@ const browserSync = require('browser-sync').create();
 
 // Path
 const path = {
-    www: {
-        style: 'www/style/',
-        html : 'www/*.html'
-    },
-    src: {
-        style: 'src/styles/*.scss'
-    },
-    watch: {
-        srcStyle   : 'src/styles/**/*.scss',
-        buildStyle : 'www/style/*.css',
-        html       : 'www/*.html',
-        js         : 'www/scripts/**/*.js'
-    }
+  www: {
+    style: 'dist/style/',
+    html: 'dist/*.html'
+  },
+  src: {
+    style: 'src/styles/*.scss'
+  },
+  watch: {
+    srcStyle: 'src/styles/**/*.scss',
+    buildStyle: 'dist/style/*.css',
+    html: 'dist/*.html',
+    js: 'dist/scripts/**/*.js'
+  }
 }
 
 // Compilation less
 function styles() {
-    return src(path.src.style)
-        .pipe(sourcemaps.init())
-        .pipe(sass()
-            .on('error', notify.onError({
-                message: '<%= error.fileName %>' +
-                '\nLine <%= error.lineNumber %>:' +
-                '\n<%= error.message %>',
-                title  : '<%= error.plugin %>'
-            }))
-        )
-        .pipe(sourcemaps.write())
-        .pipe(dest(path.www.style));
+  return src(path.src.style)
+    .pipe(sourcemaps.init())
+    .pipe(sass()
+      .on('error', notify.onError({
+        message: '<%= error.fileName %>' +
+          '\nLine <%= error.lineNumber %>:' +
+          '\n<%= error.message %>',
+        title: '<%= error.plugin %>'
+      }))
+    )
+    .pipe(sourcemaps.write())
+    .pipe(dest(path.www.style));
 }
 exports.styles = styles;
 
 function serve() {
-    browserSync.init({
-        server: "./www"
-    });
+  browserSync.init({
+    server: "./dist"
+  });
 
-    watch([
-        path.watch.srcStyle
-    ], styles);
+  watch([
+    path.watch.srcStyle
+  ], styles);
 
-    watch([
-        path.watch.html,
-        path.watch.buildStyle,
-        path.watch.js,
-    ]).on('change', reload);
+  watch([
+    path.watch.html,
+    path.watch.buildStyle,
+    path.watch.js,
+  ]).on('change', reload);
 }
 
 function reload() {
-    browserSync.reload();
+  browserSync.reload();
 }
 
 exports.default = series(
